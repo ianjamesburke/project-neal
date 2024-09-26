7f49843b1e2f78b5b6ee0bdd1383a1315e743452
ianjamesburke <37986506+ianjamesburke@users.noreply.github.com>
2024-09-26 01:38:24 -0700

    testing more

--- api/index.py
+++ api/index.py
@@ -6,25 +6,14 @@ app = Flask(__name__)
 @app.route("/api/python")
 def hello_world():
 
-    # write a tmp file to the /tmp directory
-    with open("/tmp/test.txt", "w") as f:
-        f.write("Hello, World!")
-    
     import requests
-    from io import BytesIO
-    from PIL import Image
+    url = "https://utfs.io/f/mWiSbu5B60JISbnWtmTbIuWJV8nyPDQFXv0YmcE4dfi9HLTS"
+    r = requests.get(url, allow_redirects=True)
+    open('tmp/input.wav', 'wb').write(r.content)
 
-    # download the file from the url
-    url = "https://utfs.io/f/mWiSbu5B60JIta8T6SnKzNZBbl4YLFm6rt7D8EuTWRiGQSC5"
-    response = requests.get(url)
-    img = Image.open(BytesIO(response.content))
+    with open('tmp/input.wav', 'rb') as f:
+        data = f.read()
+    
+    return "<p>file read/write seems to have worked</p>"
 
-    # trim the image
-    img = img.crop((100, 100, 400, 400))
 
-    # save the trimmed image
-    img.save("/tmp/trimmed_image.jpg")
-
-    print("file written to /tmp/trimmed_image.jpg")
-        
-    return "<p>file written to /tmp/test.txt</p>"
