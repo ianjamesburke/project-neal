78b69df209d442572c50065ee324bc332e892ecd
ianjamesburke <37986506+ianjamesburke@users.noreply.github.com>
2024-10-04 22:55:39 -0700

    added mobile view block - still need to hide layout

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from 'react';
 import Sidebar from './Sidebar';
 import Header from './Header';
@@ -10,9 +12,12 @@ export default function SpliceLayout({ children }: { children: React.ReactNode }
       <div className="flex flex-grow overflow-hidden">
         <Sidebar />
         <div className="w-px border-r border-neutral-800" />
-        <main className="flex-grow overflow-auto p-6">
+        <main className="flex-grow overflow-auto p-6 hidden md:block">
           {children}
         </main>
+        <div className="flex-grow flex items-center justify-center md:hidden">
+          <h1 className="text-center text-white mx-24"> We're sorry, but our app is currently not optimized for mobile. Please come back and visit us on a desktop.</h1>
+        </div>
       </div>
     </div>
   );
