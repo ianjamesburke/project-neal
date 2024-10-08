4007ab394795f23798122555047ab05c08754c77
ianjamesburke <37986506+ianjamesburke@users.noreply.github.com>
2024-10-07 21:36:18 -0700

    added VERCEL_URL to config

--- next.config.js
+++ next.config.js
@@ -41,6 +41,15 @@ const nextConfig = {
       },
     ];
   },
+
+  // Added environment variables
+  env: {
+    KINDE_SITE_URL: process.env.KINDE_SITE_URL ?? `https://${process.env.VERCEL_URL}`,
+    KINDE_POST_LOGOUT_REDIRECT_URL:
+      process.env.KINDE_POST_LOGOUT_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}`,
+    KINDE_POST_LOGIN_REDIRECT_URL:
+      process.env.KINDE_POST_LOGIN_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}/dashboard`
+  }
 };
 
 module.exports = nextConfig;
