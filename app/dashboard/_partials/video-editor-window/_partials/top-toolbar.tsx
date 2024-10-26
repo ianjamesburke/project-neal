f7986b3efb813a5e9114f4a505f2cc2927192c3a
aliblackeye <ali.blackeye@hotmail.com>
2024-10-26 22:37:46 +0300

    editor window

--- app/dashboard/_partials/video-editor-window/_partials/top-toolbar.tsx
+++ app/dashboard/_partials/video-editor-window/_partials/top-toolbar.tsx
@@ -4,7 +4,7 @@ import { ChevronLeft, ChevronRight, CopyPlus, Trash2 } from "lucide-react";
 export const TopToolbar = () => {
   return (
     <div className="flex h-12 w-full items-center justify-between gap-1 p-4">
-      <span className="mr-2 text-base text-gray-dark">
+      <span className="mr-2 text-base text-gray-dark ">
         Variant File Name...
       </span>
       <div className="flex gap-2">
